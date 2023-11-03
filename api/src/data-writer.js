174a761260a622d0dbbbda389a30179438fbbbf7
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-11-03 10:50:25 +0100

    fix data-extractr

--- api/src/data-writer.js
+++ api/src/data-writer.js
@@ -2,17 +2,13 @@ import path from "path";
 import fs from "fs";
 import { marked } from "marked";
 import { JSDOM } from 'jsdom';
-import get_data from "./data-extracter.js";
+import get_data from "./utils/data-extracter.js";
 
 const ROOT = path.join(process.cwd(),'../readme.md');
 const TARGET = path.join(process.cwd(),"./data/data.json");
-console.log(TARGET)
+
 const readme=fs.readFileSync(ROOT,"utf-8")
 const document=new JSDOM(marked.parse(readme)).window.document
-  
-
-
 const data=get_data(document)
 fs.writeFileSync(TARGET,JSON.stringify(data,null,2))
 
-//console.log(data)
